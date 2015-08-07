4e63524f88a6dc2d669b81d7a631191d7f6c053f
suncycle <55820659@qq.com>
2015-08-07 18:45:43 +0000

    done static pages

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -4,11 +4,20 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    assert_select "title","Home|Ruby Sample App"
   end
 
   test "should get help" do
     get :help
     assert_response :success
+    assert_select "title","Help|Ruby Sample App"
   end
+  
+  test "should get about" do
+    get :about
+    assert_response:success
+    assert_select "title","About|Ruby Sample App"
+  end
+  
 
 end
